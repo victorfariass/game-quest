2f96802b1fa0cfb54d0586d4564744bc52244b38
victor Farias <victorfarias.tv@gmail.com>
2021-09-23 09:47:20 -0300

    Updates

---
+++ precache-manifest.8c44257e23e60384af246ed3fb312d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40ed5387180d9516d87e43ba1ddd45d",
+    "url": "/game-quest/index.html"
+  },
+  {
+    "revision": "fefb57e2296c6740fc2b",
+    "url": "/game-quest/static/css/main.53421982.chunk.css"
+  },
+  {
+    "revision": "e9c09d1e87384bc28c7a",
+    "url": "/game-quest/static/js/2.19199d91.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/game-quest/static/js/2.19199d91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fefb57e2296c6740fc2b",
+    "url": "/game-quest/static/js/main.79cb1da3.chunk.js"
+  },
+  {
+    "revision": "1595d11eb4257768e4f3",
+    "url": "/game-quest/static/js/runtime-main.113b2254.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2770f56e1b51d4c91afa6fb09b03d27.js"
+  "/game-quest/precache-manifest.8c44257e23e60384af246ed3fb312d36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-quest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
